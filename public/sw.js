57959d8ca454c9fc635e5f20c0588dce679e6363
damzola <136012414+damzola@users.noreply.github.com>
2025-01-16 19:55:32 +0100

    new2

--- public/sw.js
+++ public/sw.js
@@ -27,7 +27,7 @@ this.addEventListener("fetch", (event)=>{
                 }  
                 let requestId = event.request.clone();
                     fetch(requestId)
-                    
+
                 return fetch(event.request).catch(()=>{
                     return new Response("An error as occured", {
                         status:500,
@@ -42,41 +42,3 @@ this.addEventListener("fetch", (event)=>{
     }
    
 });
-
-// let cacheValue = "pwa-v1";
-
-// self.addEventListener("install", (e) => {
-//   e.waitUntil(
-//     caches.open(cacheValue).then((cache) => {
-//       return cache.addAll([
-//         'static/js/bundle.js',
-//         'index.html',
-//         'index.css',
-//         'index.js',
-//         'favicon.ico',
-//         'static/js/main.js',
-//       ]);
-//     }).catch((err) => {
-//       console.error("Cache addAll failed:", err);
-//     })
-//   );
-// });
-
-// self.addEventListener("fetch", (event) => {
-//   event.respondWith(
-//     caches.match(event.request).then((res) => {
-//       if (res) {
-//         return res; // Return cached response if found
-//       }
-
-//       // Fallback to network if not cached
-//       return fetch(event.request).catch(() => {
-//         // Fallback response for network errors
-//         return new Response("Something went wrong", {
-//           status: 500,
-//           statusText: "Network error",
-//         });
-//       });
-//     })
-//   );
-// });
